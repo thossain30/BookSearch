428379c625b7f09ff70d4a3d27ba227eea8c8cf9
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-06-27 20:30:09 -0400

    Started on resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,44 @@
 const { Book , User } = require("../models");
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth')
 
 const resolvers = {
     Query: {
-        me: async () => {
-            return await Book.find({});
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({})
+                .select('-__v -password')
+                .populate('books')
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in')
         }
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return {token, user};
+        },
+        login: async (parent, {email, password}) => {
+            const user = await User.findOne({email});
+            if (!user) {
+              throw new AuthenticationError('Incorrect email');
+            }
+        
+            const correctPw = await user.isCorrectPassword(password);
+        
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect password');
+            }
+            const token = signToken(user);
+            return {token, user}
+        }, 
+        saveBook: async(parent, {input}, context) => {
+
+        } 
     }
 }
 
