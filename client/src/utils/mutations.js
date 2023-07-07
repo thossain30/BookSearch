7a8dc2dd4150f1593bc32128cdc6d0621241131b
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-07-07 19:15:06 -0400

    Fixing GraphQL errors

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,6 @@
 import { gql } from 'graphql-tag'
 
 export const LOGIN_USER = gql `
-{
     mutation login($email: String!, $password: String!) {
         login(email: $email, password: $password) {
             token
@@ -11,11 +10,9 @@ export const LOGIN_USER = gql `
             }
         }
     }
-}
 `;
 
 export const ADD_USER = gql `
-{
     mutation addUser($username: String!, $email: String!, $password: String!) {
         addUser(username: $username, email: $email, password: $password) {
             token
@@ -25,11 +22,9 @@ export const ADD_USER = gql `
             }
         }
     }
-}
 `;
 
 export const SAVE_BOOK = gql `
-{
     mutation saveBook($book: BookInput) {
         saveBook(book: $book) {
             _id
@@ -45,11 +40,9 @@ export const SAVE_BOOK = gql `
             }
         }
     }
-}
 `;
 
 export const REMOVE_BOOK = gql `
-{
     mutation removeBook($bookID: String!) {
         removeBook(bookId: $bookId) {
             _id
@@ -66,5 +59,4 @@ export const REMOVE_BOOK = gql `
             }
         }
     }
-}
 `;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,7 +1,6 @@
 import { gql } from '@apollo/client'
 
 export const GET_ME = gql `
-{
     query me {
         _id
         username
@@ -17,5 +16,4 @@ export const GET_ME = gql `
             description
         }
     }
-}
 `;
